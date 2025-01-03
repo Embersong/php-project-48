07474430df36c7e7924c3a5b71a72f2e4d9e8bac
ember <evebay@mail.ru>
2025-01-04 01:36:05 +0700

    Differ flat json files. Readme fix.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Embersong/php-project-48/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Embersong/php-project-48/actions)
+
+### Github Actions and code Climate:
 <a href="https://codeclimate.com/github/Embersong/php-project-48/maintainability"><img src="https://api.codeclimate.com/v1/badges/e572053c353d8e2139d5/maintainability" /></a>
 
 ## Prerequisites
@@ -20,3 +22,9 @@ cd php-project-48
 make install
 ```
 
+### Help
+```
+gendiff -h
+```
+##### Demo №1:
+[![asciicast](https://asciinema.org/a/LfQ2zdfz7Er16jyLpsPRRTWDI.svg)](https://asciinema.org/a/LfQ2zdfz7Er16jyLpsPRRTWDI)
--- src/Differ.php
+++ src/Differ.php
@@ -2,10 +2,14 @@
 
 namespace Differ\Differ;
 
-function genDiff(string $file1, string $file2, string $format = 'stylish'): void
+use function Functional\sort;
+
+function genDiff(string $file1, string $file2, string $format = 'stylish'): string
 {
     $contentFile1 = parseFile($file1);
     $contentFile2 = parseFile($file2);
+
+    return findDiff($contentFile1, $contentFile2);
 }
 
 function parseFile(string $file): array
@@ -15,4 +19,41 @@ function parseFile(string $file): array
     }
     $content = file_get_contents($file);
     return json_decode($content, true);
+}
+
+function findDiff(array $file1, array $file2): string
+{
+    $uniqueKeys = array_unique(array_merge(array_keys($file1), array_keys($file2)));
+    $sortedArray = sort($uniqueKeys, function ($first, $second) {
+        return $first <=> $second;
+    });
+
+    $res =  array_map(function ($key) use ($file1, $file2) {
+        $value1 = boolToString($file1[$key] ?? null);
+        $value2 = boolToString($file2[$key] ?? null);
+
+        if (!array_key_exists($key, $file1)) {
+            return "+ {$key}: {$value2}";
+        }
+
+        if (!array_key_exists($key, $file2)) {
+            return "- {$key}: {$value1}";
+        }
+
+        if ($value1 === $value2) {
+            return "  {$key}: {$value1}";
+        }
+
+        return "- {$key}: {$value1}" . PHP_EOL .  "+ {$key}: {$value2}";
+    }, $sortedArray);
+    return implode(PHP_EOL, $res) . PHP_EOL;
+}
+
+function boolToString(mixed $string): mixed
+{
+    if (is_bool($string)) {
+        return $string ? 'true' : 'false';
+    }
+
+    return $string;
 }
